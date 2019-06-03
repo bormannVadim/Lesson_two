9471f878b6dda3ae883fd126cb0b1ed49586ff70
bormannVadim <freedom-mv47@rambler.ru>
2019-06-03 16:38:39 +0300

    Не так назвал, не Sum, а кол-во цифр в числе,

--- Lesson_two/getSumOfNumbers/Program.cs
+++ Lesson_two/getSumOfNumbers/Program.cs
@@ -7,8 +7,22 @@ namespace getSumOfNumbers
     {
         static void Main(string[] args)
         {
-            //получаем сумму чисел
+            //Савенко Вадим
+            //найти кол-во цифр в числе
+            Console.WriteLine("Введите целое число!");
+            Console.WriteLine("Кол-во цифр в числе: " + CalculateNumeralInNumber(Convert.ToInt32(Console.ReadLine())));
+        }
 
+        static int CalculateNumeralInNumber(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return CalculateNumeralInNumber(number / 10) +1;
+            }
         }
     }
 }
