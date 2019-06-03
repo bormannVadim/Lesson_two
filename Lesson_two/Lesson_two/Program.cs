eb9c98186ebd99537acb0d0ba8bcd96d36f776d3
bormannVadim <freedom-mv47@rambler.ru>
2019-06-03 16:11:44 +0300

    Сделал программу по вычислению меньшего из 3 чисел

--- Lesson_two/Lesson_two/Program.cs
+++ Lesson_two/Lesson_two/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson_two
 {
@@ -10,7 +6,26 @@ namespace Lesson_two
     {
         static void Main(string[] args)
         {
-            Console.Title = "Hello world!";
+            // Савенко Вадим
+            // Минимальное из трёх чисел
+
+            Console.WriteLine("Введите 3 целых числа:");
+            int[] Numbers = new int[3];
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Минимальное число: "+GetMin(Numbers));
+        }
+
+        private static int GetMin(int[] Numbers)
+        {
+            int min = Numbers[0];
+            for (int i = 1; i < Numbers.Length; i++)
+            {
+                min = min < Numbers[i] ? min:Numbers[i];
+            }
+            return min;
         }
     }
 }
