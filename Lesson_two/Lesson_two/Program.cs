5c05f2ca5948b3838be4533552d33551e00f6cd9
bormannVadim <freedom-mv47@rambler.ru>
2019-06-03 15:46:59 +0300

    Добавил Hello world to title

---
+++ Lesson_two/Lesson_two/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello world!";
+        }
+    }
+}
