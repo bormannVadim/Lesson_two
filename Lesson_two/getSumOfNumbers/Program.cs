6144541dd07b4e494cbb9ecec3671ad383f41113
bormannVadim <freedom-mv47@rambler.ru>
2019-06-03 16:21:02 +0300

    Добавил новый проект, который будет считать сумму цифр числа

---
+++ Lesson_two/getSumOfNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace getSumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //получаем сумму чисел
+
+        }
+    }
+}
