0cf6ee0d44ae444bc1c206df25a30900053a06bf
bormannVadim <freedom-mv47@rambler.ru>
2019-06-03 16:50:30 +0300

    Сделал 3 задание

---
+++ Lesson_two/TillZero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TillZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+            Console.WriteLine("Вводите целые числа, пока не введёте 0!");
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number % 2 != 0 && number > 0)
+                {
+                    sum += number;
+                }
+       
+            } while (number != 0);
+            Console.WriteLine("Сумма нечетных положительных чисел: " + sum);
+        }
+    }
+}
