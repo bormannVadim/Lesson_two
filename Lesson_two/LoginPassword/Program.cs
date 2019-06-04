f0549f66604e56f5710b77a9d2ee259bda5b4880
bormannVadim <freedom-mv47@rambler.ru>
2019-06-04 09:19:36 +0300

    Добавил проект по проверке логина

---
+++ Lesson_two/LoginPassword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LoginPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Савенко Вадим
+            int attempts = 0;
+            string loging, password;
+            do {
+                Console.WriteLine("Введите логин:");
+                loging = Console.ReadLine();
+                Console.WriteLine("Введите пароль:");
+                password = Console.ReadLine();
+                attempts++;
+                if (checkPassword(loging, password))
+                {
+                    Console.WriteLine("Доступ предоставлен!");
+                    break;
+                }
+                else
+                    Console.WriteLine("В доступе отказано!\nПопробуйте ещё раз...");
+            } while (attempts < 4);
+        }
+
+        static bool checkPassword(string login, string password)
+        {
+            string EtalonLoging = "root";
+            string EtalonPassword = "GeekBrains";
+            return login == EtalonLoging && password == EtalonPassword ?true:false;
+
+        }
+    }
+}
