f1b71a154e1233c28dd82ea19ed1095741409aff
bormannVadim <freedom-mv47@rambler.ru>
2019-06-05 10:12:27 +0300

    Сделал задание про вес (5)

--- Lesson_two/task5/Program.cs
+++ Lesson_two/task5/Program.cs
@@ -20,16 +20,9 @@ namespace task5
             double Weight = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите свой рост в см.");
             double Height = Convert.ToDouble(Console.ReadLine())/100;
+
             Calculate(Weight, Height);
-
-            if (Fitness(Weight, Height) > 0)// набрать
-            {
-
-            }
-            else // похудеть
-            {
-
-            }
+        
         }
 
         static void Calculate(double weight, double height)
@@ -40,7 +33,10 @@ namespace task5
             else if (coefficient > 16 && coefficient <= 18.5)
                 Console.WriteLine("У вас дефицит коллорий! Вам нужно  начинать есть...");
             else if (coefficient > 18.5 && coefficient <= 25)
+            {
                 Console.WriteLine("У вас всё хорошо!");
+                return ;
+            }
             else if (coefficient > 25 && coefficient <= 30)
                 Console.WriteLine("У вас избыточный вес! Пора бы задуматься о диете...");
             else if (coefficient > 30 && coefficient <= 35)
@@ -48,12 +44,41 @@ namespace task5
             else if (coefficient > 35 && coefficient <= 40)
                 Console.WriteLine("Вы близитесь к состоянию 'жирный'! Немедленно займитесь собой...");
             else
-               Console.WriteLine("Вы жирный! Вам нужно срочно похудеть...");
+                Console.WriteLine("Вы жирный! Вам нужно срочно похудеть...");
+
+            Fitness(weight, height);
         }
 
-        static double Fitness(double weight, double height)
+        static void Fitness(double weight, double height)
         {
-            return 0;
+           
+            double UsualWeight = weight;
+            double coefficient;
+            do
+            {
+                coefficient = weight / Math.Pow(height, 2);
+                if (coefficient <= 18.5)
+                {
+                    weight++;
+                }
+                else
+                {
+
+                    weight--;
+               }
+            }
+            while (coefficient <= 18.5 || coefficient > 25);
+      
+            double result = UsualWeight-weight ;
+
+            if (result < 0)// похудеть
+            {
+                Console.WriteLine("Вам нужно набрать: " + Math.Abs(result) + " кг.");
+            }
+            else // набрать
+            {
+                Console.WriteLine("Вам нужно сбросить: " + result + " кг.");
+            }
         }
     }
 }
