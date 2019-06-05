78fff5a9903dedae7006824f5ab8a77b69629452
bormannVadim <freedom-mv47@rambler.ru>
2019-06-05 11:01:23 +0300

    7 -  только первая часть

---
+++ Lesson_two/Задание_7_рекурсивный_метод/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Задание_7_рекурсивный_метод
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Савенко Вадим
+            Console.WriteLine("Введите a");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            FromAtoB(a, b);
+        }
+
+        static void FromAtoB(int a, int b)
+        {
+            if (a >= b)
+            {
+                return;
+            }
+            else
+            {
+                    a++;
+                    if (a == b)
+                    return;
+                    else
+                    Console.WriteLine(a);
+           
+                FromAtoB(a, b);
+            }
+        }
+    }
+}
